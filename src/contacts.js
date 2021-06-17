e7010dfea91777684c07897bc76fb631c7ca255c
Shivam Bhardwaj <50479634+Shivambhardwaj18@users.noreply.github.com>
2021-06-17 16:27:54 +0530

    first commit

---
+++ src/contacts.js
@@ -0,0 +1,10 @@
+const contacts = [
+  {
+    name: "Shivam Bhardwaj",
+    imgURL: "/Profile1.jpg",
+    phone: "+91 xxxxxxxxxx",
+    email: "shivambhardawaj0129@gmail.com",
+  },
+];
+
+export default contacts;
